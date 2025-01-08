624eaecb43b49ff3f977fc39b5ce7ea4c9450454
Junior Sacarias Garcia <junior.sacarias.garcia.diaz@volenday.com>
2025-01-08 09:47:47 +0000

    fix: git Guardian

--- bin/main.dart
+++ bin/main.dart
@@ -6,7 +6,7 @@ void main() async {
     const envFilePath = '.env';
 
     // Token proporcionado directamente
-    //const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlLZXkiOiIzMjQ3NjY0ZS1iODRiLTQ5OTAtOThmNC1kNjFiNTFiYTA1NDQiLCJhcHBsaWNhdGlvbiI6eyJJZCI6IjI3YzhlYjIwLThlNDEtMTFlYy04YTU5LTY3MDE1Njk5MWRlNiIsIlNjaGVtYU5hbWUiOiIyN2M4ZWIyMC04ZTQxLTExZWMtOGE1OS02NzAxNTY5OTFkZTYifSwiYWNjb3VudCI6eyJQZXJzb25JZCI6NjUsIlVzZXJJZCI6NjZ9LCJleHBpcmF0aW9uIjoiMWgiLCJpYXQiOjE3MzQ2MTg2ODksImV4cCI6MTczNDYyMjI4OX0.BcAvb-3aQTH_o73G-c7rh0N-rElrYXe1IBt1YZdyyQM';
+    //const token = '';
 
     // Definir el entorno actual (cambiar a 'test' o 'prod' según el caso)
     const environment = 'dev';
